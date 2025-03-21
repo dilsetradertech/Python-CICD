a48c7b10a622d55c867643b6e900a9fd7bf320e9
deepaklearnkaro <learnkarodeepak@gmail.com>
2025-03-21 15:04:07 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
@@ -7,10 +5,11 @@ app = FastAPI()
 
 users_db = {}
 
+
 class UserUpdate(BaseModel):
     name: str
-    email: EmailStr 
-    
+    email: EmailStr
+
 
 @app.post("/update-user/{user_id}")
 def update_user(user_id: int, user: UserUpdate):
@@ -25,6 +24,7 @@ def get_user(user_id: int):
         return {"user_id": user_id, "name": user["name"], "email": user["email"]}
     return {"error": "User not found"}
 
-@app.get('/')
+
+@app.get("/")
 async def root():
-    return {'message': 'Hello World hello there'}
+    return {"message": "Hello World hello there"}
